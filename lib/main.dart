39d4a2464be9d03e70a4cce619c4ca3467a309bb
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-22 08:35:13 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:payment_app/Features/Checkout/Presentation/Views/my_cart_view.dart';
+
+void main() {
+  runApp(const CheckOutApp());
+}
+
+class CheckOutApp extends StatelessWidget {
+  const CheckOutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyCartView(),
+    );
+  }
+}
