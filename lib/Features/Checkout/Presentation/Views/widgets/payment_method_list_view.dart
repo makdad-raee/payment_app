12a2fb0944aed4a5174e2ba2a6c431476c5ea4bb
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-23 13:04:04 -0800

    select payment method make it green

--- lib/Features/Checkout/Presentation/Views/widgets/payment_method_list_view.dart
+++ lib/Features/Checkout/Presentation/Views/widgets/payment_method_list_view.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/Features/Checkout/Presentation/Views/widgets/payment_methode_item.dart';
 
-class PaymentMethodeListView extends StatelessWidget {
+class PaymentMethodeListView extends StatefulWidget {
   const PaymentMethodeListView({super.key});
+
+  @override
+  State<PaymentMethodeListView> createState() => _PaymentMethodeListViewState();
+}
+
+class _PaymentMethodeListViewState extends State<PaymentMethodeListView> {
   final List<String> paymentMethodeItems = const [
     'assets/images/cart.svg',
     'assets/images/paypal.svg',
     'assets/images/applepay.svg'
   ];
+  int activeIndex=0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +26,17 @@ class PaymentMethodeListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: PaymentMethodeItem(
-              isActive: false,
-              image: paymentMethodeItems[index],
+            child: GestureDetector(
+              onTap: () {
+                activeIndex=index;
+                setState(() {
+                  
+                });
+              },
+              child: PaymentMethodeItem(
+                isActive: activeIndex==index,
+                image: paymentMethodeItems[index],
+              ),
             ),
           );
         },
