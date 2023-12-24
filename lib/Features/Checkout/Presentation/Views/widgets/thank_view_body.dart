5878c0a3461e686fd3f594d869d501a9c5c3d6dd
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-24 15:00:14 -0800

    create dash  line in the middle of circle avatar by list.generate

--- lib/Features/Checkout/Presentation/Views/widgets/thank_view_body.dart
+++ lib/Features/Checkout/Presentation/Views/widgets/thank_view_body.dart
@@ -8,6 +8,7 @@ class ThankYouViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(30.0),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
@@ -18,13 +19,53 @@ class ThankYouViewBody extends StatelessWidget {
             ),
           ),
           Positioned(
+            left: 20+8,
+            right: 20+8,
+            bottom: MediaQuery.sizeOf(context).height * 0.2+20,
+            child: Row(
+              children: List.generate(
+                30,
+                (index) => Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal:2),
+                    child: Container(
+                      height: 2,
+                      
+                      color: const Color(0xFFB8B8B8),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
             left: -20,
-            bottom: MediaQuery.sizeOf(context).height*0.2,
-            child: CircleAvatar(backgroundColor: Colors.white,),),
-            Positioned(
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
             right: -20,
-            bottom: MediaQuery.sizeOf(context).height*0.2,
-            child: CircleAvatar(backgroundColor: Colors.white,),),
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          const Positioned(
+            top: -50,
+            left: 0,
+            right: 0,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color(0xFFD9D9D9),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Color(0xFF34A853),
+                child: Icon(Icons.check, size: 50),
+              ),
+            ),
+          )
         ],
       ),
     );
