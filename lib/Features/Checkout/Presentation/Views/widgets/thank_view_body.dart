6cc296f7a4d6958cd46ff8e4fd62a0b6fadd2b0c
makdad-raee <makdadraeeeeee@gmail.com>
2023-12-24 14:16:11 -0800

    add half circle avatar to right and lift button of thank u screen

--- lib/Features/Checkout/Presentation/Views/widgets/thank_view_body.dart
+++ lib/Features/Checkout/Presentation/Views/widgets/thank_view_body.dart
@@ -5,6 +5,28 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -20,
+            bottom: MediaQuery.sizeOf(context).height*0.2,
+            child: CircleAvatar(backgroundColor: Colors.white,),),
+            Positioned(
+            right: -20,
+            bottom: MediaQuery.sizeOf(context).height*0.2,
+            child: CircleAvatar(backgroundColor: Colors.white,),),
+        ],
+      ),
+    );
   }
 }
